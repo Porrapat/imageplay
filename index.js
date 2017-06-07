d3083f4a1f7537863d48f05f57785d5a77321bed
Porrapat Petchdamronskul <porrapat.cpe20@gmail.com>
2017-06-07 13:37:48 +0700

    OK Can Loop Code.2

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ function pad(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-
 var i = 0;
 
 // loop for reading the same image until process will be killed/exited
-
-function loop(filename_to_read) {
+function loop(filename_to_read,i) {
     console.log('%s', filename_to_read);
     Jimp.read("src/BJPG/"+filename_to_read).then(function(lenna) {
 		
-		console.log(filename_to_read + " Writed");
+		console.log(i + "." + filename_to_read + " Writed");
 		lenna.write("dist/BJPG/"+filename_to_read); // save 
 		
         // loop(filename);
@@ -26,31 +24,10 @@ function loop(filename_to_read) {
 
 // read images
 var i=1;
-
-for (i=1;i <= 10;i++) {
+for (i=1;i <= 300;i++) {
 	var filename_to_read = 'STAR'+pad(i,3)+' copy.jpg';
-	loop(filename_to_read);
+	loop(filename_to_read,i);
 }
-
-/*
-var i=1;
-
-for (i=1;i <= 3;i++) {
-	// STAR001 copy.jpg
-	// console.log('STAR'+pad(i,3)+' copy.jpg');
-	
-	var filename_to_read = 'STAR'+pad(i,3)+' copy.jpg';
-	console.log(filename_to_read);
-	Jimp.read("src/BJPG/"+filename_to_read, function (err, lenna) {
-		if (err) throw err;
-		// lenna.resize(256, 256)         // resize 
-		//	 .quality(60)                 // set JPEG quality 
-		//	 .greyscale()                 // set greyscale 
-		console.log(filename_to_read + " Writed.");
-		lenna.write("dist/BJPG/"+filename_to_read); // save 
-	});
-}
-*/
 
 /*
 Jimp.read("src_test/lenna.jpg", function (err, lenna) {
